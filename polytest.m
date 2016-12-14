94c90e39cd175965236f3216aba26075aa9a9de3
Stefan Rickli <stefanrickli@gmx.ch>
2016-12-14 07:37:36 +0100

    Put calculation of Poly-Coefficients into a function.

--- polytest.m
+++ polytest.m
@@ -8,17 +8,21 @@ function polytest
     dx_inv = 2/(a-b);
     m = -(a+b)/2;
     
-    c = cell(16,1);
-    c{1} = [(ya-yb)/(a-b),(a*yb-b*ya)/(a-b)];
-    for ii = 2:2:16
-        c{ii} = -dx_inv^ii .* binomialcoeffs(ii,m) + [zeros(1,ii),1];
-    end
+    c = initPolyCoeffs(a,b,ya,yb,dx_inv,m);
     
     t = a:(b-a)/1000:b;
     plot(t,polyval(c{4},t)-polyval(c{2},t));
 %     ylim(yb*[-1 1]);
 %     line((a+b)/2*[1 1],yb*[-1 1],'linewidth',0.1);
     
+end
+
+function c = initPolyCoeffs(a,b,ya,yb,dx_inv,m)
+    c = cell(16,1);
+    c{1} = [(ya-yb)/(a-b),(a*yb-b*ya)/(a-b)];
+    for ii = 2:2:16
+        c{ii} = -dx_inv^ii .* binomialcoeffs(ii,m) + [zeros(1,ii),1];
+    end    
 end
 
 function res = binomialcoeffs(degree,m)
