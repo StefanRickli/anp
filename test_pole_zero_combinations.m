14526907a5417d0498fc61ef91aa8bc8a4a26e13
Stefan Rickli <stefanrickli@gmx.ch>
2016-12-17 21:59:56 +0100

    Added documentation.

--- test_pole_zero_combinations.m
+++ test_pole_zero_combinations.m
@@ -6,14 +6,24 @@ function pole_zero_combinations = test_pole_zero_combinations(imaginary_PolesZer
 %     secant_max_zero = 1/10;
 %     
 %     imaginary_PolesZeros_sorted = test_struct_sort(poles,zeros);
+
     
+    % this ratio determines which fraction of the distance between the
+    % neighboring pole/zero is used for the secant of its detour
     secant_max_ratio = secant_max_pole/secant_max_zero;
 
+    % initialize the struct array to hold the information
     pole_zero_combinations = repmat(struct('type','xx','distance',NaN,'secant_pole',NaN,'secant_zero',NaN), length(imaginary_PolesZeros_sorted)-1, 1 );
+    
+    % iterate through every pairwise pole/zero combination along the
+    % imaginary axis
     for ii = 2:length(imaginary_PolesZeros_sorted)
+        % determine the type of pole or zero combination
         pole_zero_combinations(ii-1).type = [imaginary_PolesZeros_sorted(ii-1).type,imaginary_PolesZeros_sorted(ii).type];
+        % what's the distance between them?
         pole_zero_combinations(ii-1).distance = abs(imaginary_PolesZeros_sorted(ii-1).value - imaginary_PolesZeros_sorted(ii).value);
 
+        % calculate the secant lengths according to the secant ratio.
         switch pole_zero_combinations(ii-1).type
             case 'pp'
                 pole_zero_combinations(ii-1).secant_pole = pole_zero_combinations(ii-1).distance/2;
@@ -27,6 +37,6 @@ function pole_zero_combinations = test_pole_zero_combinations(imaginary_PolesZer
                 pole_zero_combinations(ii-1).secant_zero = pole_zero_combinations(ii-1).distance / (secant_max_ratio + 1);
             otherwise
                 error('Oops, we shouldn''t be here. Apologies! Please report this crash to ricklis@student.ethz.ch together with the input you used.');
-        end 
+        end
     end
 end
--- test_struct_sort.m
+++ test_struct_sort.m
@@ -1,27 +1,44 @@
+% Accepts two arrays containing the pole and zero values.
+% Returns a struct that contains the sorted pole and zero location and
+% denotes which is of which type.
 function poles_zeros_sorted = test_struct_sort(poles,zeros)
+    % first find all the pure imaginary poles and zeros
     poles = poles(real(poles) == 0);
     zeros = zeros(real(zeros) == 0);
     
+    % strip them off the imaginary unit
     poles = imag(poles);
     zeros = imag(zeros);
-
+    
+    % remember how many purely imaginary poles and zeros we have
     npoles = length(poles);
     nzeros = length(zeros);
-
+    
+    % initialize the struct array to hold the information, courtesy to this
+    % efficient way of initializing the struct array:
     % http://stackoverflow.com/questions/13664090/how-to-initialize-an-array-of-structs-in-matlab
     poles_zeros_sorted = repmat(struct('type','a','value',[]), npoles+nzeros, 1 );
-
-    p = repmat({'p'},npoles,1);
-    pp = mat2cell(poles,1,ones(1,npoles));
-    z = repmat({'z'},nzeros,1);
-    zz = mat2cell(zeros,1,ones(1,nzeros));
+    
+    % fill the struct array with the pole/zero locations
+    % in order to avoid for-loops, convert the num arrays first to cell
+    % arrays, whose multiple outputs we then can use to fill the
+    % corresponding struct array fields.
+    p = repmat({'p'},npoles,1);                 % cell array full of 'p'
+    pp = mat2cell(poles,1,ones(1,npoles));      % cell array containing pole values
+    z = repmat({'z'},nzeros,1);                 % cell array full of 'z'
+    zz = mat2cell(zeros,1,ones(1,nzeros));      % cell array containing zero values
     [poles_zeros_sorted(1:npoles).type] = p{:};
     [poles_zeros_sorted(1:npoles).value] = pp{:};
     [poles_zeros_sorted(npoles+1:end).type] = z{:};
     [poles_zeros_sorted(npoles+1:end).value] = zz{:};
-
+    
+    % Use the function below to sort the rows by the 'value'-field. Flip
+    % its output in order to get a descending sorted list.
+    % Now we know in which order the poles and zeros appear on the
+    % imaginary axis.
     poles_zeros_sorted = flipud(sort_struct_array_by_field_index(poles_zeros_sorted,2));
 end
+
 % code adopted from
 % http://blogs.mathworks.com/pick/2010/09/17/sorting-structure-arrays-based-on-fields/
 function sorted_struct_array = sort_struct_array_by_field_index(array_in,ind)
