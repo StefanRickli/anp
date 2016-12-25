4c327500e59f70a24b07bb18d5b42e839ec65691
Stefan Rickli <stefanrickli@gmx.ch>
2016-12-25 23:04:17 +0100

    Removed old code: D-shape, in-value calculation.

--- animated_nyquist_plot.m
+++ animated_nyquist_plot.m
@@ -301,8 +301,10 @@ function [] = main(animationParams)
         pz_im = pz_all(real(pz_all) == 0);
         animationParams.R = max([abs(R1),abs(R2),abs(pz_im)*1.5]);
     end
-    g = polesZeros2fncHandle(animationParams.tf_zeros, animationParams.tf_poles);
     
+    % prepare the transfer function
+    g = @(z) polyval(poly(zeros),z)./polyval(poly(poles),z);
+
     %% preparations
     % prepare frame buffer
     animationParams.N_frames = animationParams.duration * animationParams.FPS;
@@ -322,16 +324,11 @@ function [] = main(animationParams)
     end
     
     %% calculate the function values
-%     in = getInputFunctionHandle();
-    %out = @(t,R,phi) g(in(t,animationParams));
-    
-    %in_values = in(t,animationParams.R,animationParams.phi);
-%     [ts,in_values] = get_t_and_shape(animationParams);
     radii.inf = animationParams.R;
     angles.crop = animationParams.phi*pi/180;
     angles.detour = 45*pi/180;
-    in_values = test_evaluate_input_function(t,poles,zeros,radii,angles);
     
+    in_values = test_evaluate_input_function(t,poles,zeros,radii,angles);
     out_values = g(in_values);
     
     %% calculation of the plot(s)
@@ -503,18 +500,9 @@ function [frames] = drawFunctions(in_values, out_values, zeros, poles, R, t, t_i
         subplot(sub2); out_plot_arrow = annotation('line',[0 0],[1 0]);
 
         % We need to know the value of the head of the trail at the previous
-        % frame in order to determine the arrow's direction. In the case of the
-        % exact start direction, we need the value at the second to last frame
-        % because if we took the last frame then we'd use f(t=1) = f(t=0).
-        % For the debug case when t_min > 0 or t_max < 1 we simply
-        % extrapolate from the second frame backwards.
-        if t(1) == 0 && t(end) == 1
-            in_values_head_prev = in_values(animationParams.resolution_factor-1);
-            out_values_head_prev = out_values(animationParams.resolution_factor-1);
-        else
-            in_values_head_prev = 2*in_values(1) - in_values(2);
-            out_values_head_prev = 2*out_values(1) - out_values(2);
-        end
+        % frame in order to determine the arrow's direction.
+        in_values_head_prev = in_values(animationParams.resolution_factor-1);
+        out_values_head_prev = out_values(animationParams.resolution_factor-1);
 
         % prepare the annotations below the plots and remember their handles
 
@@ -690,135 +678,6 @@ function arrowHandle = drawTextArrow(x0, phi, l, text, color)
     set(arrowHandle,'parent',gca,'position',[x0-r,r],'String',text,'Color',color);
 end
 
-%% input function shapes
-% t is in the following range 0<=t<=1.
-% the shape should be continuous and a closed curve (i.e. shape(0)=shape(1))
-function inputFunctionHandle = getInputFunctionHandle()
-    %inputFunctionHandle = @(t) unitCircle(t);
-    %inputFunctionHandle = @(t) dCurve(t);
-    inputFunctionHandle = @(t,R,phi) roundedCurve(t,R,phi);
-end
-
-% this D-shaped curve has small arcs at the edges to smooth them and
-% make the derivative of the curve continuous
-% The shape is basically split into 5 parts to which the indexes correspond
-% The two parameters one can play with are phi, which determines the degree
-% of roundoff (0<phi<180) and the radius of the big arc R.
-function [t, shape] = get_t_and_shape(animationParams)
-    poles_zeros = [animationParams.tf_zeros animationParams.tf_poles];
-    phi = animationParams.phi;
-    R = animationParams.R;
-    
-    detour_interval_max = 1/2;
-    detour_angle = 45;
-    
-    t_min = animationParams.t_min;
-    t_max = animationParams.t_max;
-    
-    assert(t_min >= 0 && t_min <1 && t_max > 0 && t_max <= 1 && t_min < t_max);
-    t_step = (t_max-t_min)/(animationParams.N_frames-1);
-    t = t_min:t_step:t_max;
-    
-    n_steps = length(t);
-    
-    phi = deg2rad(phi);
-    
-    % calculate values of the small round-off circles
-    r = R*sin(phi)/(1+sin(phi));
-    m2 = r + 1i*sqrt(R^2-2*R*r);
-    m4 = r - 1i*sqrt(R^2-2*R*r);
-    
-    % need to know how long the parts are
-    d1 = sqrt(R^2-2*R*r);
-    d2 = r*(pi/2 + phi);
-    d3 = R*(pi - 2*phi);
-    d4 = d2;
-    d5 = d1;
-    
-    % length of the round part (small arcs + big arc)
-    d_arcs = d2 + d3 + d4;
-    
-    % calculate the t-values at which two adjacent regions touch
-    % the only arbitrary choice here is t12. together with t45 it
-    % determines the fraction of time we need for the y-axis part of the
-    % D-curve.
-    % the v-values are velocity-factors
-    t12 = 2/5; 
-    t45 = 1-t12;
-    v_ave = d_arcs / (t45 - t12);
-    
-    t23 = t12 + d2/v_ave;
-    t34 = 1-t23;
-    
-    v11 = 3; % again the only arbitrary choice amongst the velocity-factors. determines the speed of the animation near the origin.
-    v12 = (d1-v11*t12)/t12^4;
-    v51 = v11;
-    v52 = v12;
-        
-    ts = t_min:t_step:t_max;    % '1st row': t-values
-    shape = zeros(n_steps,1);   % '2nd row': shape-values
-    
-    region1_idx = find(ts >=t_min          & ts <= min(t_max,t12));
-    region2_idx = find(ts > max(t_min,t12) & ts <= min(t_max,t23));
-    region3_idx = find(ts > max(t_min,t23) & ts <= min(t_max,t34));
-    region4_idx = find(ts > max(t_min,t34) & ts <= min(t_max,t45));
-    region5_idx = find(ts > max(t_min,t45) & ts <=t_max);
-    
-    shape(region1_idx) = (v11*ts(region1_idx) + v12*ts(region1_idx).^4)*1i; % part 1
-    shape(region2_idx) = m2 + r*exp(1i*(pi - (ts(region2_idx)-t12).*(pi/2 + phi)/(t23-t12))); % part 2
-    shape(region3_idx) = R*exp(1i*(pi/2 - phi - (ts(region3_idx)-t23).*(pi-2*phi)/(t34-t23))); % part 3
-    shape(region4_idx) = m4 + r*exp(1i*(phi - pi/2 - (ts(region4_idx)-t34).*(pi/2 + phi)/(t45-t34))); % part 4
-    shape(region5_idx) = -(v51*(1-ts(region5_idx)) + v52*(1-ts(region5_idx)).^4)*1i; % part 5
-    
-    % get poles and zeros which lie on the imaginary axis
-    imag_pz_idx = find(real(poles_zeros) == 0);
-    if isempty(imag_pz_idx)
-        return;
-    end
-    
-    imaginary_pz = poles_zeros(imag_pz_idx);
-    
-    % find minimum distance between any pair of only-imaginary poles and
-    % zeros
-    mask = triu(ones(length(imaginary_pz)),1);
-    [pz1,pz2] = meshgrid(imaginary_pz,imaginary_pz);
-    pairwise_distances = abs(pz1.*mask - pz2.*mask);
-    d_min = min(nonzeros(pairwise_distances));
-    if isempty(d_min)
-        d_min = 1e100; % high value such that detour_interval_max will be chosen
-    end
-    
-    % determine the radii for the detours 
-    % 1)    based on the calculated minimum distances between imaginary
-    %       poles and zeros
-    % 2)    based on the provided maximal secant length
-    detour_angle = detour_angle*pi/180;
-    max_pairwise_detour_radius = sqrt((d_min/2)^2/(1-tan(detour_angle)^2/(4+tan(detour_angle)^2)));
-    max_parametrized_detour_radius =  sqrt((detour_interval_max/2)^2/(1-tan(detour_angle)^2/(4+tan(detour_angle)^2)));
-    
-    % set the parameters for all of the detour circles
-    secant_detour = min(d_min,detour_interval_max);
-    radius_detour = min(max_pairwise_detour_radius,max_parametrized_detour_radius);
-    x_shift_detour = sqrt(radius_detour^2 - (secant_detour/2)^2);
-    
-    for ii = 1:length(imaginary_pz)
-        detour_interval_low = imag(imaginary_pz(ii))-secant_detour/2;
-        detour_interval_high = imag(imaginary_pz(ii))+secant_detour/2;
-        detour_point_idx = find(real(shape) == 0 & imag(shape) > detour_interval_low & imag(shape) < detour_interval_high);
-        shape(detour_point_idx) = shifted_circle(shape(detour_point_idx),imaginary_pz(ii));
-    end
-    
-    
-    function z = shifted_circle(t,y_shift_detour)
-        z = sqrt(radius_detour^2-imag(t-y_shift_detour).^2) - x_shift_detour + imag(t).*1i;
-    end
-end
-
-%% transfer function preparation
-function g = polesZeros2fncHandle(zeros,poles)
-    g = @(z) polyval(poly(zeros),z)./polyval(poly(poles),z);
-end
-
 %% various stuff
 % this function clips the input values to the maximum value allowed inside
 % the plot
@@ -874,7 +733,7 @@ function legacy = checkMatlabVersion()
 end
 
 % moves the current ax(l)es into the origin of the plot
-function [] = setaxes_origin(legacy)
+function setaxes_origin(legacy)
     switch(legacy)
         case 'R2015b_or_newer'
             ax = gca;
