ccc1f2fd1661605a55f1b53d89a44b9c425fb7a8
Stefan Rickli <stefanrickli@gmx.ch>
2017-02-24 10:53:18 +0100

    Catch any exception and cleanup the tf_processor object. This is to ensure that we're not using potentially faulty data the next time anp_main is run.

--- anp_main.m
+++ anp_main.m
@@ -94,35 +94,39 @@ function [] = anp_main(varargin)
     debug_graphics = true;
     debug_graphics_interpolation = false;
     debug_text = false;
-
+    
     addpath('anp_files');
     addpath('anp_icons');
-
-    args =                      anp_parse_arguments(varargin{:});
-
+    
+    args = anp_parse_arguments(varargin{:});
+    
     % if argument checking has passed, get previous GUI and TF-Processing
     % objects back or re-initialize them if they have been deleted in the
     % meantime
     persistent h_tf_processor
-
-    h_gui = anp_gui;
-
-    if isempty(h_tf_processor) || ~isvalid(h_tf_processor)
-        h_tf_processor =        anp_tf_processor;
-    else
-        fprintf('Reusing old TF processor instance\n');
+    
+    try
+        h_gui = anp_gui;
+        
+        if isempty(h_tf_processor) || ~isvalid(h_tf_processor)
+            h_tf_processor =        anp_tf_processor;
+        else
+            fprintf('Reusing old TF processor instance\n');
+        end
+        
+        args.gui_handle =           h_gui;
+        args.processor_handle =     h_tf_processor;
+        args.tf_poles =             roots(args.tf_obj.Numerator{1})';
+        args.tf_zeros =             roots(args.tf_obj.Denominator{1})';
+        
+        h_tf_processor.init_params(args);
+        
+        h_gui.init_params(args);
+        
+        h_gui.init_visuals();
+    catch err
+        clear h_tf_processor;
+        disp(getReport(err,'extended'));
     end
-
-    args.gui_handle =           h_gui;
-    args.processor_handle =     h_tf_processor;
-    args.tf_poles =             roots(args.tf_obj.Numerator{1})';
-    args.tf_zeros =             roots(args.tf_obj.Denominator{1})';
-
-    h_tf_processor.init_params(args);
-
-    h_gui.init_params(args);
-
-    h_gui.init_visuals();
-
 end
 
