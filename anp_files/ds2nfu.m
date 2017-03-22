48dbf2f81cc282eed5efa13e365d94623e85b0db
Stefan Rickli <stefanrickli@gmx.ch>
2017-03-22 21:51:46 +0100

    Fix (trail head) arrow annotations separating when zoomed in far
    Up to now an arrow's tip and shaft would separate when its parent
    plot is zoomed beyond 0.05 per grid delta.
    (I regard this as a bug in Matlab's API.)
    
    This fix changes the annotation's parent object to the figure itself.
    As the figure's coordinates are relative in the range [0,1]x[0,1], we
    need to perform a coordinate transform first. Use DS2NFU.m for that
    purpose.
    
    Note that the text arrow annotations of outlying p/z are still affected
    by the separation bug, but this is acceptable as nobody probably will
    zoom into the z-plot that far.
    
    Reported this ill-behavior to Mathworks.
    https://ch.mathworks.com/matlabcentral/answers/331372-potential-bug-in-annotation-arrow-with-parent-gca-when-zoomed-in-far
    
    Fixes #2

---
+++ anp_files/ds2nfu.m
@@ -0,0 +1,24 @@
+function [x_t,y_t] = ds2nfu(hAx,x,y)
+    % DS2NFU  Convert data space units into normalized figure units.
+    %
+    % Michelle Hirsch
+    % mhirsch@mathworks.com
+    % Copyright 2006-2014 The MathWorks, Inc
+    %
+    % Shortened by Stefan Rickli
+    
+    %% Get limits
+    axun = get(hAx,'Units');
+    set(hAx,'Units','normalized');
+    axpos = get(hAx,'Position');
+    axlim = axis(hAx);
+    axwidth = diff(axlim(1:2));
+    axheight = diff(axlim(3:4));
+    
+    %% Transform data
+    x_t = (x-axlim(1))*axpos(3)/axwidth + axpos(1);
+    y_t = (y-axlim(3))*axpos(4)/axheight + axpos(2);
+    
+    %% Restore axes units
+    set(hAx,'Units',axun)
+end
