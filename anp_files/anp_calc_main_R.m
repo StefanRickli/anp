41d9ca8bc79c8f3fdc4d39b5adcc01dd8d339f9d
Stefan Rickli <stefanrickli@gmx.ch>
2017-03-23 23:26:32 +0100

    Enable basic delay plotting capability
    One can now specify delays in tf-objects.
    
    Either directly use exponential notation:
    s = tf('s'); G=1/(s+1)*exp(-2*s);
    or specify the delay by directly changing the tf's property:
    G.InputDelay = 10;
    
    There are still large issues with the scaling of the D-contour radius
    and the spatial resolution in the low frequency domain.
    
    The text boxes below the plots still need to be updated.

--- anp_files/anp_calc_main_R.m
+++ anp_files/anp_calc_main_R.m
@@ -1,4 +1,4 @@
-function R = anp_calc_main_R(poles,zeros,min_angle_contribution_at_R,max_pole_separation)
+function R = anp_calc_main_R(poles,zeros,min_angle_contribution_at_R,max_pole_separation,delay)
     min_R = 2.5 * max_pole_separation;
     
     pz_all = [poles,zeros];
@@ -8,4 +8,13 @@ function R = anp_calc_main_R(poles,zeros,min_angle_contribution_at_R,max_pole_se
     
     pz_im = pz_all(abs(real(pz_all)) < 100*eps);
     R = max([abs(R1),abs(R2),abs(pz_im)*1.5,min_R]);
+    
+    if delay ~= 0
+        R = 2*R + max(0,exp(-1.5*log(delay))) + max(0,100*log(delay)) + 15;
+    end
+    
+    if R > 1e4
+        warning('Calculation of D-shape radius returned %f. Are you really shure?\nComputation can take a considerable amount of time and memory!\nConsider aborting by closing the empty figure, otherwise press any key to continue and THEN close the figure.\n',R);
+        waitforbuttonpress;
+    end
 end
