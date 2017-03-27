e0e520af9d3acfd050b7f422e7f2250759ae71e2
Stefan Rickli <stefanrickli@gmx.ch>
2017-03-27 13:48:46 +0200

    Amend documentation

--- anp_files/anp_d_contour_init_and_evaluate.m
+++ anp_files/anp_d_contour_init_and_evaluate.m
@@ -1,11 +1,27 @@
-function z = anp_in_fct_init_and_evaluate(t,in_params)
+function z = anp_d_contour_init_and_evaluate(t,in_params)
+    % Serves as main entry point for the creation D-contour points based on a set of input data points that are in an interval of [0,1]
+    % 
+    % PRE:  - t is a vector with monotonically increasing values from 0 to 1
+    %       - all necessary parameters are set in in_params
+    % POST: returns a vector containing the points of a close D-contour
+    %       curve that begin in the origin and end in the origin
+    
+    % Check if 'debug_graphics' has already been set and if not, do it
+    % standalone
     if any(strcmp(who('global'),'debug_graphics'))
-        global debug_graphics;
+        global debug_graphics;                                  %#ok<TLEV>
     else
         debug_graphics = false;
     end
     
+    % Sort the pure imaginary poles and zeros in the order they will be
+    % encountered en route of the D-contour
     in_data.im_pz_sorted = anp_in_fct_sort_pz(in_params);
+    
+    % Determines which combinations of pole-zero occur in which order and
+    % calculates the distance between the neighbors.
+    % This distances are later used to form an upper bound on the radii we
+    % can use for the detours.
     in_data.im_pz_combinations = anp_in_fct_pz_combinations(in_params,in_data);
     
     [in_data.interval_list,...
