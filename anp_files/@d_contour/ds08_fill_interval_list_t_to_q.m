79faf59081e0527f4e8cbf6a6baad69cb047e368
Stefan Rickli <stefanrickli@gmx.ch>
2017-03-30 15:15:12 +0200

    Isolate 'exp_Lorentz' into separate file

--- anp_files/@d_contour/ds08_fill_interval_list_t_to_q.m
+++ anp_files/@d_contour/ds08_fill_interval_list_t_to_q.m
@@ -120,21 +120,4 @@ function shares = determine_crop_inf_shares(shares,interval_list)
     shares.inf =  inf_arc_length/crop_inf_arc_length * shares.crop_inf;
 end
 
-function z = exp_Lorentz(T,a,b,ya,c_lorentz,gamma_star,r1,r2,c1,c2)
-    delta_x = b - a;
-    
-    if ~isnan(c_lorentz)
-        Lorentz_0 = c_lorentz  *  (atan( (2*T-a-b) ./ (2*gamma_star) ) + ...
-                                   atan(   delta_x / (2*gamma_star) ) + ...
-                                   (4*gamma_star.*(a-T)) ./ (delta_x^(2)+4*gamma_star^2));
-    else
-        Lorentz_0 = zeros(size(T));            
-    end
 
-    G1 = c1 * (a*exp(-r1*b) + exp(-r1*a)/r1 - exp(-r1*T)/r1 - T*exp(-r1*b));
-    G2 = c2 * (a*exp( r2*a) - exp( r2*a)/r2 + exp( r2*T)/r2 - T*exp( r2*a));
-    G = G1 + G2;
-
-    z = ya+G+Lorentz_0;
-
-end
