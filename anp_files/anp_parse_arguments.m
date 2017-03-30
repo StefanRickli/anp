f035f4c3c483e5ea14bb13edf52ce4d4d5a50a48
Stefan Rickli <stefanrickli@gmx.ch>
2017-03-30 14:44:02 +0200

    Tune the size of the minimal im-axis part between detours

--- anp_files/anp_parse_arguments.m
+++ anp_files/anp_parse_arguments.m
@@ -194,7 +194,7 @@ function checked_args = anp_parse_arguments(varargin)
     checked_args.angles.detour =        45;                 % [°]
     checked_args.halfsecants.pole_max = 1/4;                % [1] absolute
     checked_args.halfsecants.zero_max = 1/8;                % [1] absolute
-    checked_args.halfsecants.margin =   0.05;               % how much free space between the nearest neighboring poles/zeros? ==> avoid that the nearest pole/zero-detours could have no straight part between them. p.31
+    checked_args.halfsecants.margin =   0.3;                % how much free space between the nearest neighboring poles/zeros? ==> avoid that the nearest pole/zero-detours could have no straight part between them. p.31
     checked_args.weights.pole =         2;                  % if a pole is on the Im-axis, how many spatial points should it be given in the nyquist plot?
     checked_args.weights.zero =         1/3;                % same as above
     
