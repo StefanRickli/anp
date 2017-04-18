8487c310b98861e61c4570d6ee510cf117f9583f
Stefan Rickli <stefanrickli@gmx.ch>
2017-04-18 16:00:41 +0200

    Prettify code (order of factors) in 'ds07h_negative_pz.m'
    - Put value factors in front of boolean factors to improve readability.
    - Separate function handle arguments.
    
    The hope is that we get a faster grasp of how the code works.

--- anp_files/@d_contour/ds07h_negative_pz.m
+++ anp_files/@d_contour/ds07h_negative_pz.m
@@ -20,7 +20,8 @@ function [interval_ii,idx_current_pz,prev_upper_bound] = ds07h_negative_pz(this,
                                            repmat('detour_zero', im_pz_sorted(idx_current_pz).zero)];
         interval_list(interval_ii).q(1) = prev_upper_bound;
         
-        interval_length = im_pz_sorted(idx_current_pz).pole*arc_lengths.detour_pole + im_pz_sorted(idx_current_pz).zero*arc_lengths.detour_zero;
+        interval_length = arc_lengths.detour_pole * im_pz_sorted(idx_current_pz).pole + ...
+                          arc_lengths.detour_zero * im_pz_sorted(idx_current_pz).zero;
         interval_list(interval_ii).q_len = interval_length;
         
         interval_list(interval_ii).q(2) = interval_list(interval_ii).q(1) + interval_length;
@@ -28,9 +29,15 @@ function [interval_ii,idx_current_pz,prev_upper_bound] = ds07h_negative_pz(this,
         
         switch im_pz_sorted(idx_current_pz).type
             case 'p'
-                interval_list(interval_ii).input_fct_handle = @(q) circ_detour(map(q,interval_list(interval_ii).q(1),interval_list(interval_ii).q(2),0,arc_lengths.detour_pole),radii.detour_pole,secant_pole,im_pz_sorted(idx_current_pz).value);
+                interval_list(interval_ii).input_fct_handle = @(q) circ_detour(map(q,interval_list(interval_ii).q(1),interval_list(interval_ii).q(2),0,arc_lengths.detour_pole), ...
+                                                                               radii.detour_pole, ...
+                                                                               secant_pole, ...
+                                                                               im_pz_sorted(idx_current_pz).value);
             case 'z'
-                interval_list(interval_ii).input_fct_handle = @(q) circ_detour(map(q,interval_list(interval_ii).q(1),interval_list(interval_ii).q(2),0,arc_lengths.detour_zero),radii.detour_zero,secant_zero,im_pz_sorted(idx_current_pz).value);
+                interval_list(interval_ii).input_fct_handle = @(q) circ_detour(map(q,interval_list(interval_ii).q(1),interval_list(interval_ii).q(2),0,arc_lengths.detour_zero), ...
+                                                                               radii.detour_zero, ...
+                                                                               secant_zero, ...
+                                                                               im_pz_sorted(idx_current_pz).value);
         end
         
         dbg_out('interval\t[%.3f\t%.3f],\tlength = %.3f,\tdetour_neg\n',interval_list(interval_ii).q(1),interval_list(interval_ii).q(2),interval_length);
@@ -40,14 +47,22 @@ function [interval_ii,idx_current_pz,prev_upper_bound] = ds07h_negative_pz(this,
             interval_list(interval_ii).type = 'axis';
             interval_list(interval_ii).q(1) = prev_upper_bound;
             
-            interval_length = pole_zero_combinations(idx_current_pz).distance - halfsecant_pole*(im_pz_sorted(idx_current_pz).pole + im_pz_sorted(idx_current_pz+1).pole) - halfsecant_zero*(im_pz_sorted(idx_current_pz).zero + im_pz_sorted(idx_current_pz+1).zero);            
+            interval_length = pole_zero_combinations(idx_current_pz).distance + ...
+                              -halfsecant_pole * ( im_pz_sorted(idx_current_pz).pole + ...
+                                                   im_pz_sorted(idx_current_pz+1).pole) + ...
+                              -halfsecant_zero * ( im_pz_sorted(idx_current_pz).zero + ...
+                                                   im_pz_sorted(idx_current_pz+1).zero);
             interval_list(interval_ii).q_len = interval_length;
             
             interval_list(interval_ii).q(2) = interval_list(interval_ii).q(1) + interval_length;
             prev_upper_bound = interval_list(interval_ii).q(2);
             
-            za = im_pz_sorted(idx_current_pz).value + im_pz_sorted(idx_current_pz).pole*halfsecant_pole + im_pz_sorted(idx_current_pz).zero*halfsecant_zero;
-            zb = im_pz_sorted(idx_current_pz+1).value - im_pz_sorted(idx_current_pz+1).pole*halfsecant_pole - im_pz_sorted(idx_current_pz+1).zero*halfsecant_zero;
+            za = im_pz_sorted(idx_current_pz).value + ...
+                 halfsecant_pole * im_pz_sorted(idx_current_pz).pole + ...
+                 halfsecant_zero * im_pz_sorted(idx_current_pz).zero;
+            zb = im_pz_sorted(idx_current_pz+1).value + ...
+                 -halfsecant_pole * im_pz_sorted(idx_current_pz+1).pole + ...
+                 -halfsecant_zero * im_pz_sorted(idx_current_pz+1).zero;
             interval_list(interval_ii).input_fct_handle = @(q) im_axis_line(q,interval_list(interval_ii).q(1),interval_list(interval_ii).q(2),za,zb);
 
             dbg_out('interval\t[%.3f\t%.3f],\tlength = %.3f,\tlinear_neg\n',interval_list(interval_ii).q(1),interval_list(interval_ii).q(2),interval_length);
