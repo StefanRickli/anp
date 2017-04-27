76f41914183d31cee13951e261398bfd72e1df4c
Stefan Rickli <stefanrickli@gmx.ch>
2017-04-27 16:27:51 +0200

    Add 'anp_mimo_test.m' to unit tests

--- run_unit_tests.m
+++ run_unit_tests.m
@@ -2,6 +2,7 @@ tests = {};
 
 tests{end+1} = testsuite('unit_tests/anp_d_contour_test.m');
 tests{end+1} = testsuite('unit_tests/anp_usage_examples_test.m');
+tests{end+1} = testsuite('unit_tests/anp_mimo_test.m');
 tests{end+1} = testsuite('unit_tests/anp_random_test.m');
 
 runner = matlab.unittest.TestRunner.withTextOutput;
