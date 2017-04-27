990991bee5e30153cda106886775feaca9d54a24
Stefan Rickli <stefanrickli@gmx.ch>
2017-04-27 11:26:18 +0200

    Adapt paths to unit tests in 'run_unit_tests.m'

--- run_unit_tests.m
+++ run_unit_tests.m
@@ -1,5 +1,5 @@
-tests_d_contour =       testsuite('anp_d_contour_test.m');
-tests_usage_examples =  testsuite('anp_usage_examples_test.m');
+tests_d_contour =       testsuite('unit_tests/anp_d_contour_test.m');
+tests_usage_examples =  testsuite('unit_tests/anp_usage_examples_test.m');
 
 runner = matlab.unittest.TestRunner.withTextOutput;
 
