4655a73a4711969db57015306c823a694c6a687d
Stefan Rickli <stefanrickli@gmx.ch>
2017-07-17 00:40:22 +0200

    Add more documentation to 'run_unit_tests.m'

--- run_unit_tests.m
+++ run_unit_tests.m
@@ -1,3 +1,5 @@
+%% define test files to be run
+
 tests = {};
 
 tests{end+1} = testsuite('unit_tests/anp_d_contour_test.m');
@@ -5,13 +7,16 @@ tests{end+1} = testsuite('unit_tests/anp_usage_examples_test.m');
 tests{end+1} = testsuite('unit_tests/anp_mimo_test.m');
 tests{end+1} = testsuite('unit_tests/anp_random_test.m');
 
+%% init TestRunner
 runner = matlab.unittest.TestRunner.withTextOutput;
 
+%% run tests
 res = {};
 for ii = 1:length(tests)
     res{end+1} = runner.run(tests{ii});
 end
 
+%% display results
 disp('-----------------------------------------------------------------------------------')
 
 for ii = 1:length(res)
